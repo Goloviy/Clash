e565291cdf74f0e370fd0561243d9f14fd722d61
Goloviy <goloviy@tut.by>
2024-10-23 08:28:20 +0300

    star add localization

--- Assets/ScriptLocalization.cs
+++ Assets/ScriptLocalization.cs
@@ -16,6 +16,20 @@ namespace I2.Loc
 			public static string tit_1 		{ get{ return LocalizationManager.GetTranslation ("ChapterData/tit.1"); } }
 			public static string tit_2 		{ get{ return LocalizationManager.GetTranslation ("ChapterData/tit.2"); } }
 		}
+
+		public static class EquipmentOp
+		{
+			public static string Skill_CrossBow_1 		{ get{ return LocalizationManager.GetTranslation ("EquipmentOp/Skill.CrossBow.1"); } }
+			public static string Skill_Pistol_2 		{ get{ return LocalizationManager.GetTranslation ("EquipmentOp/Skill.Pistol.2"); } }
+			public static string Skill_Pistol_1 		{ get{ return LocalizationManager.GetTranslation ("EquipmentOp/Skill.Pistol.1"); } }
+			public static string Skill_CrossBow_5 		{ get{ return LocalizationManager.GetTranslation ("EquipmentOp/Skill.CrossBow.5"); } }
+			public static string Skill_CrossBow_4 		{ get{ return LocalizationManager.GetTranslation ("EquipmentOp/Skill.CrossBow.4"); } }
+			public static string Skill_CrossBow_3 		{ get{ return LocalizationManager.GetTranslation ("EquipmentOp/Skill.CrossBow.3"); } }
+			public static string Skill_CrossBow_2 		{ get{ return LocalizationManager.GetTranslation ("EquipmentOp/Skill.CrossBow.2"); } }
+			public static string Skill_Pistol_3 		{ get{ return LocalizationManager.GetTranslation ("EquipmentOp/Skill.Pistol.3"); } }
+			public static string Skill_Pistol_4 		{ get{ return LocalizationManager.GetTranslation ("EquipmentOp/Skill.Pistol.4"); } }
+			public static string Skill_Pistol_5 		{ get{ return LocalizationManager.GetTranslation ("EquipmentOp/Skill.Pistol.5"); } }
+		}
 	}
 
     public static class ScriptTerms
@@ -31,6 +45,20 @@ namespace I2.Loc
 		    public const string tit_3 = "ChapterData/tit.3";
 		    public const string tit_1 = "ChapterData/tit.1";
 		    public const string tit_2 = "ChapterData/tit.2";
+		}
+
+		public static class EquipmentOp
+		{
+		    public const string Skill_CrossBow_1 = "EquipmentOp/Skill.CrossBow.1";
+		    public const string Skill_Pistol_2 = "EquipmentOp/Skill.Pistol.2";
+		    public const string Skill_Pistol_1 = "EquipmentOp/Skill.Pistol.1";
+		    public const string Skill_CrossBow_5 = "EquipmentOp/Skill.CrossBow.5";
+		    public const string Skill_CrossBow_4 = "EquipmentOp/Skill.CrossBow.4";
+		    public const string Skill_CrossBow_3 = "EquipmentOp/Skill.CrossBow.3";
+		    public const string Skill_CrossBow_2 = "EquipmentOp/Skill.CrossBow.2";
+		    public const string Skill_Pistol_3 = "EquipmentOp/Skill.Pistol.3";
+		    public const string Skill_Pistol_4 = "EquipmentOp/Skill.Pistol.4";
+		    public const string Skill_Pistol_5 = "EquipmentOp/Skill.Pistol.5";
 		}
 	}
 }
---
+++ Assets/TestLocal.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using I2.Loc;
+using TMPro;
+using UnityEngine;
+
+public class TestLocal : MonoBehaviour
+{
+    [SerializeField] private TMP_Text text;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        var atr = ScriptLocalization.EquipmentOp.Skill_CrossBow_1;
+        var str = LocalizationManager.GetTranslation("EquipmentOp/Skill.CrossBow.1");
+        var str2 = LocalizationManager.GetTranslation("EquipmentOp/Skill.Sword.1");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
